dc829c85d0e534b4e854a3f1d0ab5004b799ba0a
story <story801216@yahoo.com.tw>
2021-08-19 11:04:27 +0800

    增加navbar 刪除原本的html nav

--- pinchiao/hospital_2/index.php
+++ pinchiao/hospital_2/index.php
@@ -96,33 +96,7 @@ $rows = $pdo->query($sql)->fetchAll();
     <title>找醫院</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-            </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php">找醫院</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="add.html">新增資料</a>
-                </li>
-            </ul>
-        </div>
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="login.php">登入</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">註冊</a>
-            </li>
-        </ul>
-    </div>
-</nav>
+<?php include __DIR__. '/navbar.php'; ?>
 
     <div class="info text-center">
         <!-- <h1>醫院資訊</h1> -->
