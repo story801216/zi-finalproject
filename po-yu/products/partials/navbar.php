75f3c05ab73f868213b2c8341d4ef4600a216a2e
story <story801216@yahoo.com.tw>
2021-08-19 10:48:21 +0800

    與梓庭的navber完成同步

--- po-yu/products/partials/navbar.php
+++ po-yu/products/partials/navbar.php
@@ -28,29 +28,32 @@
                 </li>
 
 
-
             </ul>
 
             <ul class="navbar-nav">
             <?php if(isset($_SESSION['user'])): ?>
-                <li class="nav-item active">
-                    <a class="nav-link" ><?= $_SESSION['user']['name'] ?></a> <!--把href拿掉的用意在於登入後['nickname']就沒有可點入的部分 -->
-                 </li>
-                <li class="nav-item active">
-                    <a class="nav-link" href="../../../finalproject/zi-ting/login.php">登入</a>
-                </li>
-
-                <li class="nav-item">
-                    <a class="nav-link" href="../../../finalproject/zi-ting/logout.php">登出</a>
-                </li>
-
-
+                    <li class="nav-item active">
+                         <a class="nav-link" ><?= $_SESSION['user']['name'] ?></a> <!--把href拿掉的用意在於登入後['nickname']就沒有可點入的部分 -->
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="profile-edit.php">編輯個人資料</a> 
+                    </li> 
+                    <li class="nav-item">
+                        <a class="nav-link" href="../../../finalproject/zi-ting/logout.php">登出</a>
+                    </li>
+                    <?php else: ?>
+                        <li class="nav-item active">
+                        <a class="nav-link" href="../../../finalproject/zi-ting/login.php">登入</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="data-insert.php">註冊</a>
+                    </li>
+                <?php endif; ?>
                 <li class="nav-item active cart-icon">
                         <a class="nav-link" href="cart-check.php" >
                             <i class="fas fa-shopping-cart"><?= isset($_SESSION['total']) ? $_SESSION['total']: ''; ?></i>
                         </a>
                 </li>
-               <?php endif; ?>
             </ul>
 
         </div>
