929ee876d43ad0e8ed3259e9760e0d6208f1d2b9
story <story801216@yahoo.com.tw>
2021-08-19 10:57:49 +0800

    改h2為C組小專

--- zi-ting/index_.php
+++ zi-ting/index_.php
@@ -5,7 +5,7 @@
 <?php include __DIR__. '/partials/html-head.php'; ?>
 <?php include __DIR__. '/partials/navbar.php'; ?>
 <div class="container">
-    <h2>Hello PHP</h2>
+    <h2>C組小專</h2>
 </div>
 <?php include __DIR__. '/partials/scripts.php'; ?>
 <?php include __DIR__. '/partials/html-foot.php'; ?>
