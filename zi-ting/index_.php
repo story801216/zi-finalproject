9625a4b7825a1271752705c877faad7b6e638d1b
story <story801216@yahoo.com.tw>
2021-08-20 18:39:36 +0800

    增加首頁圖片

--- zi-ting/index_.php
+++ zi-ting/index_.php
@@ -1,11 +1,22 @@
 <?php
-    include __DIR__. '/partials/init.php';
-    $title = 'C組php&sql小專';
+include __DIR__ . '/partials/init.php';
+$title = 'C組php&sql小專';
 ?>
-<?php include __DIR__. '/partials/html-head.php'; ?>
-<?php include __DIR__. '/partials/navbar.php'; ?>
+<?php include __DIR__ . '/partials/html-head.php'; ?>
+<?php include __DIR__ . '/partials/navbar.php'; ?>
 <div class="container">
-    <h2>C組小專</h2>
+    
+    <div class="pic">
+        <img src="./img/3.jpg" alt="">.
+        
+    </div>
+
+    <style>
+        .pic{
+            margin-top: 80px;
+        
+        }
+    </style>
 </div>
-<?php include __DIR__. '/partials/scripts.php'; ?>
-<?php include __DIR__. '/partials/html-foot.php'; ?>
+<?php include __DIR__ . '/partials/scripts.php'; ?>
+<?php include __DIR__ . '/partials/html-foot.php'; ?>
